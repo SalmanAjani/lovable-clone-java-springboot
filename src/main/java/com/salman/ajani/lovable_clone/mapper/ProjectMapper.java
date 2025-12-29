f297e35b47130df3b36404d8721a0e9b9637df7b
Salman <salmanajani0426@gmail.com>
2025-12-29 14:06:11 +0530

    Finished v1 project implementation

---
+++ src/main/java/com/salman/ajani/lovable_clone/mapper/ProjectMapper.java
@@ -0,0 +1,21 @@
+package com.salman.ajani.lovable_clone.mapper;
+
+import com.salman.ajani.lovable_clone.dto.project.ProjectResponse;
+import com.salman.ajani.lovable_clone.dto.project.ProjectSummaryResponse;
+import com.salman.ajani.lovable_clone.entity.Project;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface ProjectMapper {
+
+    ProjectResponse toProjectResponse(Project project);
+
+    @Mapping(target = "projectName", source = "name")
+    ProjectSummaryResponse toProjectSummaryResponse(Project project);
+
+    List<ProjectSummaryResponse> toListOfProjectSummaryResponse(List<Project> projects);
+
+}
